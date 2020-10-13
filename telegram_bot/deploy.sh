2d0089e018f4526a5ac3f7815661f60af18832ba
bringmetheaugust <bringmetheaugust@gmail.com>
2020-10-13 14:24:58 +0300

    add pip emoji

--- telegram_bot/deploy.sh
+++ telegram_bot/deploy.sh
@@ -5,4 +5,5 @@ source py_env/bin/activate &&
 pip install -U aiogram &&
 pip install -U pylint &&
 pip install -U python-dotenv &&
+pip install -U emoji &&
 deactivate
--- telegram_bot/src/index.py
+++ telegram_bot/src/index.py
@@ -1,15 +1,35 @@
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
+from emoji import emojize
 
 load_dotenv()
+
+carList = ['BWM', 'Ford', 'Audi', 'Dodge']
 
 bot = Bot(token = os.getenv('CHALLENGER_TESTING_TOKEN'))
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands = ['start'])
 async def echo(message: types.Message):
-    await message.reply(message.text)
+    # keys = types.InlineKeyboardMarkup()
+    # keys.add(list(carList))
+
+    carMenu = types.ReplyKeyboardMarkup()
+    carMenu.add(types.InlineKeyboardMarkup())
+
+    # poll_keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    # poll_keyboard.add(types.KeyboardButton(
+    #     text="Создать викторину",
+    #     request_poll=types.KeyboardButtonPollType(type = types.PollType.QUIZ))
+    # )
+    # poll_keyboard.add(types.KeyboardButton(text="Отмена"))
+
+    await message.answer(emojize('Select car\'s brand:oncoming_automobile:'))
+
+@dp.message_handler(commands = ['help'])
+async def help(message: types.Message):
+    await message.reply_markup('<b>/start</b> to create new search')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
