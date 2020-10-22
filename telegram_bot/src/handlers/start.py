c697f3d66feed70f20960e64e933af0cb7bc9523
bringmetheaugust <bringmetheaugust@gmail.com>
2020-10-22 20:31:45 +0300

    telegram bot: reset State data after new force start

--- telegram_bot/src/handlers/start.py
+++ telegram_bot/src/handlers/start.py
@@ -8,8 +8,10 @@ from components.catalogueList import catalogueList
 carList: list = ['BMW', 'Ford', 'Audi', 'Dodge', 'Reno', 'Volvo', 'Mazda', 'Porshe', 'Lamborgini', 'Lada', 'Opel', 'Chevrole', 'VolzWagen', 'Kia']
 years: list = ['2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008']
 
-@dp.message_handler(commands='start')
+@dp.message_handler(commands = 'start', state = '*')
 async def cmd_start(message: Message, state: FSMContext):
+    await state.reset_data()
+    await state.reset_state()
     await FormState.withBrands.set()
 
     mapedBrandList = list(map(lambda brand: FormItem(brand), carList))
