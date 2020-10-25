d0a468e00208f5510a0026776db606dab49c27db
bringmetheaugust <bringmetheaugust@gmail.com>
2020-10-26 01:56:37 +0200

    telegram bot: refactoring

--- telegram_bot/src/middleWare.py
+++ telegram_bot/src/middleWare.py
@@ -5,6 +5,7 @@ from aiogram.types import CallbackQuery, ParseMode
 from bot import bot
 from state import FormState, FormItem
 from components.catalogueList import catalogueList
+from constants import CONFIRM_BUTTON_CALLBACK_QUERY_TYPE
 
 class HandlerMiddleware(BaseMiddleware):
     def __init__(self):
@@ -12,30 +13,30 @@ class HandlerMiddleware(BaseMiddleware):
 
     # check confirm button
     async def on_pre_process_callback_query(self, callback: CallbackQuery, data: dict):
-        if ('confirm' in callback.data):
+        if (CONFIRM_BUTTON_CALLBACK_QUERY_TYPE in callback.data):
             state = self.manager.dispatcher.current_state()
             currentStateData = await state.get_data()
             
             for category in currentStateData:
                 # check empty category lists
                 if (not any(item.isSelected for item in currentStateData[category])):
-                    await callback.answer(
+                    await callback.bot.send_message(
                         text = '☹️Seems, You didn\'t choose anything...\n🥺Please, select at least one car brand.',
-                        show_alert = True
+                        chat_id = callback.message.chat.id,
+                        parse_mode = ParseMode.HTML
                     )
-                    raise CancelHandler()
 
                 # create new list & set State steps
                 else:
                     if ('brand' in callback.data): # ! add FormState.withBrand checking
                         await FormState.withYears.set()
                         await callback.bot.send_message(
-                            text = 'Type 1st registration year (single <b>2007</b> or range <b>2001-2009</b> e.g)🚐',
+                            text = 'Great!🚐\nType 1st registration year (single <b>2007</b> or range <b>2001-2009</b> e.g).',
                             chat_id = callback.message.chat.id,
                             parse_mode = ParseMode.HTML
                         )
 
-                    raise CancelHandler()
+            raise CancelHandler()
 
     # update catalogueList
     async def on_post_process_callback_query(self, callback: CallbackQuery, results, data: dict):
