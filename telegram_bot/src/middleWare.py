5e2d5e239ed86d681e75f445c680686e1f30e523
bringmetheaugust <bringmetheaugust@gmail.com>
2020-10-26 02:32:47 +0200

    telegram bot: move catalogue check login from middleware

--- telegram_bot/src/middleWare.py
+++ telegram_bot/src/middleWare.py
@@ -17,29 +17,20 @@ class HandlerMiddleware(BaseMiddleware):
             state = self.manager.dispatcher.current_state()
             currentStateData = await state.get_data()
             
+            # check empty category lists
             for category in currentStateData:
-                # check empty category lists
                 if (not any(item.isSelected for item in currentStateData[category])):
                     await callback.bot.send_message(
                         text = '☹️Seems, You didn\'t choose anything...\n🥺Please, select at least one car brand.',
                         chat_id = callback.message.chat.id,
                         parse_mode = ParseMode.HTML
                     )
-
-                # create new list & set State steps
-                else:
-                    if ('brand' in callback.data): # ! add FormState.withBrand checking
-                        await FormState.withYears.set()
-                        await callback.bot.send_message(
-                            text = 'Great!🚐\nType 1st registration year (single <b>2007</b> or range <b>2001-2009</b> e.g).',
-                            chat_id = callback.message.chat.id,
-                            parse_mode = ParseMode.HTML
-                        )
-
-            raise CancelHandler()
+                    raise CancelHandler()
 
     # update catalogueList
     async def on_post_process_callback_query(self, callback: CallbackQuery, results, data: dict):
+        if (not bool(results)): return
+
         updatedList, *_ = results
         
         await bot.edit_message_reply_markup(
